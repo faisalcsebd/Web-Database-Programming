b994a0b7d2cac65fb0e19fd8670e23cd903731be
Faisal Ahmed <50084163+faisalcsebd@users.noreply.github.com>
2019-11-25 22:19:19 +0600

    Add files via upload

---
+++ Final/product.php
@@ -0,0 +1,268 @@
+<?php
+include "header.php";
+?>
+		
+		<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+					event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},900);
+				});
+			});
+</script>
+		<script>
+    
+    (function (global) {
+	if(typeof (global) === "undefined")
+	{
+		throw new Error("window is undefined");
+	}
+    var _hash = "!";
+    var noBackPlease = function () {
+        global.location.href += "#";
+	
+        global.setTimeout(function () {
+            global.location.href += "!";
+        }, 50);
+    };	
+	
+    global.onhashchange = function () {
+        if (global.location.hash !== _hash) {
+            global.location.hash = _hash;
+        }
+    };
+    global.onload = function () {        
+		noBackPlease();
+
+		document.body.onkeydown = function (e) {
+            var elm = e.target.nodeName.toLowerCase();
+            if (e.which === 8 && (elm !== 'input' && elm  !== 'textarea')) {
+                e.preventDefault();
+            }
+            
+            e.stopPropagation();
+        };		
+    };
+})(window);
+</script>
+
+		
+		<div class="section main main-raised">
+			
+			<div class="container">
+		
+				<div class="row">
+				
+					
+					<?php 
+								include 'db.php';
+								$product_id = $_GET['p'];
+								
+								$sql = " SELECT * FROM products ";
+								$sql = " SELECT * FROM products WHERE product_id = $product_id";
+								if (!$con) {
+									die("Connection failed: " . mysqli_connect_error());
+								}
+								$result = mysqli_query($con, $sql);
+								if (mysqli_num_rows($result) > 0) 
+								{
+									while($row = mysqli_fetch_assoc($result)) 
+									{
+									echo '
+									
+                                    
+                                
+                                <div class="col-md-5 col-md-push-2">
+                                <div id="product-main-img">
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'" alt="">
+                                    </div>
+
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'" alt="">
+                                    </div>
+
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'" alt="">
+                                    </div>
+
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                                
+                                <div class="col-md-2  col-md-pull-5">
+                                <div id="product-imgs">
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'" alt="">
+                                    </div>
+
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'" alt="">
+                                    </div>
+
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'g" alt="">
+                                    </div>
+
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'" alt="">
+                                    </div>
+                                </div>
+                            </div>
+
+                                 
+									';
+                                    
+									?>
+									
+									
+									<?php 
+									echo '
+									
+                                    
+                   
+									
+					
+					<div class="col-md-12">
+						<div id="product-tab">
+							<!-- product tab nav -->
+							<ul class="tab-nav">
+								<li class="active"><a data-toggle="tab" href="#tab1">Description</a></li>
+								<li><a data-toggle="tab" href="#tab2">Details</a></li>
+								<li><a data-toggle="tab" href="#tab3">Reviews</a></li>
+							</ul>
+						
+							<div class="tab-content">
+								<!-- tab1  -->
+								<div id="tab1" class="tab-pane fade in active">
+									<div class="row">
+										<div class="col-md-12">
+											<p>The product quality is very good and user get the best to his/her test.</p>
+											<p><h1>All products made by Bangladesh </h1></p>
+                                            
+										</div>
+									</div>
+								</div>
+								<!-- /tab1  -->
+
+								<!-- tab2  -->
+								<div id="tab2" class="tab-pane fade in">
+									<div class="row">
+										<div class="col-md-12">
+											<p>The product quality is very good and user get the best to his/her test.</p>
+                                            <p><h1>All products made by Bangladesh </h1></p>
+										</div>
+									</div>
+								</div>
+								
+								<div id="tab3" class="tab-pane fade in">
+									<div class="row">
+										
+
+										
+									</div>
+								</div>
+							
+							</div>
+							
+						</div>
+					</div>
+					
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		
+		<div class="section main main-raised">
+			
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+                    
+					<div class="col-md-12">
+						<div class="section-title text-center">
+							<h3 class="title">Related Products</h3>
+							
+						</div>
+					</div>
+                    ';
+									$_SESSION['product_id'] = $row['product_id'];
+									}
+								} 
+								?>	
+								<?php
+                    include 'db.php';
+								$product_id = $_GET['p'];
+                    
+					$product_query = "SELECT * FROM products,categories WHERE product_cat=cat_id AND product_id BETWEEN $product_id AND $product_id+3";
+                $run_query = mysqli_query($con,$product_query);
+                if(mysqli_num_rows($run_query) > 0){
+
+                    while($row = mysqli_fetch_array($run_query)){
+                        $pro_id    = $row['product_id'];
+                        $pro_cat   = $row['product_cat'];
+                        $pro_brand = $row['product_brand'];
+                        $pro_title = $row['product_title'];
+                        $pro_price = $row['product_price'];
+                        $pro_image = $row['product_image'];
+
+                        $cat_name = $row["cat_title"];
+
+                        echo "
+				
+                        
+                                <div class='col-md-3 col-xs-6'>
+								<a href='product.php?p=$pro_id'><div class='product'>
+									<div class='product-img'>
+										<img src='product_images/$pro_image' style='max-height: 170px;' alt=''>
+										<div class='product-label'>
+											<span class='sale'>-30%</span>
+											<span class='new'>NEW</span>
+										</div>
+									</div></a>
+									<div class='product-body'>
+										<p class='product-category'>$cat_name</p>
+										<h3 class='product-name header-cart-item-name'><a href='product.php?p=$pro_id'>$pro_title</a></h3>
+										<h4 class='product-price header-cart-item-info'>$pro_price<del class='product-old-price'>$990.00</del></h4>
+										<div class='product-rating'>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+										</div>
+										<div class='product-btns'>
+											<button class='add-to-wishlist'><i class='fa fa-heart-o'></i><span class='tooltipp'>add to wishlist</span></button>
+											<button class='add-to-compare'><i class='fa fa-exchange'></i><span class='tooltipp'>add to compare</span></button>
+											<button class='quick-view'><i class='fa fa-eye'></i><span class='tooltipp'>quick view</span></button>
+										</div>
+									</div>
+									<div class='add-to-cart'>
+										<button pid='$pro_id' id='product' class='add-to-cart-btn block2-btn-towishlist' href='#'><i class='fa fa-shopping-cart'></i> add to cart</button>
+									</div>
+								</div>
+                                </div>
+							
+                        
+			";
+		}
+        ;
+      
+}
+?>
+					
+				</div>
+				
+                
+			</div>
+			
+		</div>
+		
+<?php
+
+include "footer.php";
+
+?>
